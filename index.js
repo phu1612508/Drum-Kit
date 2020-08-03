622a3eb394db5f345982af59ab8b097ba9451b56
phu1612508 <43996827+phu1612508@users.noreply.github.com>
2020-08-03 18:18:30 +0700

    finish

--- index.js
+++ index.js
@@ -1,8 +1,69 @@
+function playMusic(key) {
+  switch (key) {
+    case "w":
+      var audio = new Audio('sounds/tom-1.mp3').play();
+      break;
+
+    case "a":
+      var audio = new Audio('sounds/tom-2.mp3').play();
+      break;
+
+    case "s":
+      var audio = new Audio('sounds/tom-3.mp3').play();
+      break;
+
+    case "d":
+      var audio = new Audio('sounds/tom-4.mp3').play();
+      break;
+
+    case "j":
+      var audio = new Audio('sounds/snare.mp3').play();
+      break;
+
+    case "k":
+      var audio = new Audio('sounds/crash.mp3').play();
+      break;
+
+    case "l":
+      var audio = new Audio('sounds/kick-bass.mp3').play();
+      break;
+
+
+    default:
+      console.log(this.innerHTML);
+  }
+}
+
 document.querySelectorAll("button").forEach(function(currentValue) {
   currentValue.addEventListener("click", function() {
-    var audio = new Audio('sounds/tom-1.mp3');
-    audio.play();
+    try {
+      playMusic(this.innerHTML);
+      addButtonAnimation(this.innerHTML);
+    } catch (e) {
+      console.log("invalid key");
+    } finally {
+
+    }
+
   });
 });
 
-// document.querySelector("button").addEventListener("click",handleClick);
+document.addEventListener("keypress", function(event) {
+  try {
+    playMusic(event.key);
+    addButtonAnimation(event.key);
+  } catch (e) {
+    console.log("invalid key");
+  } finally {
+
+  }
+
+});
+
+function addButtonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function() {
+      activeButton.classList.remove("pressed");
+    }, 100);
+}
